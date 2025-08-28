9788f821f85d42ef3e1100864dd031295d727161
Tomas Pastor Salazar <63889846+magic-tps@users.noreply.github.com>
2025-08-28 00:26:55 -0500

    2
    2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/v&eciviltech/',
+  base: '/V-E-CivilTech/',
   plugins: [react(), tailwindcss(),],
 })
